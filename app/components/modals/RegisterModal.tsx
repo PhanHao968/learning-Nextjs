42c54c3fef69df498b616d086a30e42603074c7a
PhanHao968 <127004411+PhanHao968@users.noreply.github.com>
2023-09-19 23:31:46 +0700

    add heading register...

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,7 +1,69 @@
+'use client';
+import axios from 'axios';
+import {AiFillGithub} from "react-icons/ai";
+import {FcGlobe} from "react-icons/fc";
+import {useCallback, useState} from "react";
+import {
+  FieldValues,
+    SubmitHandler,
+    useForm
+} from 'react-hook-form';
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "@/app/components/modals/Modal";
+import Heading from "@/app/components/Heading";
 
 const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState:{
+      errors,
+    }
+  } = useForm<FieldValues>({
+    defaultValues:{
+      name: '',
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) =>{
+    setIsLoading(true);
+
+    axios.post('api/register', data)
+        .then(()=>{
+          registerModal.onClose();
+        })
+        .catch((errors)=>{
+          console.log(errors);
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+  }
+
+  const bodyContent = (
+      <div className="flex flex-col gap-4">
+        <Heading
+            title="Welcome to Airbnb"
+            subtitle="Create an account!"
+        />
+      </div>
+  )
+
   return(
-      <div></div>
+      <Modal
+        disable={isLoading}
+        isOpen={registerModal.isOpen}
+        title="Register"
+        actionLabel="Continue"
+        onClose={registerModal.onClose}
+        onSubmit={handleSubmit(onSubmit)}
+        body={bodyContent}
+   />
   );
 }
 
